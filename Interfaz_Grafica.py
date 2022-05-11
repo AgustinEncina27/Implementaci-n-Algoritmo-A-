dc7b21c139558854132dfafbdcae5d646ec56124
Agustin <agustin72encina@gmail.com>
2022-05-11 20:36:36 -0300

    Carga de nodos y relaciones aleatoria. Subgrafo

--- Interfaz_Grafica.py
+++ Interfaz_Grafica.py
@@ -8,39 +8,54 @@ import matplotlib.pyplot as plt
 import random
 
 
+
 #Creación del grafo
 G = nx.Graph()
 
+#Establece el formato del grafo
+def AtributosDeGrafo(G,pos):
+	#Estilos de los nodos
+	nx.draw_networkx_nodes(G, pos, node_size=200,node_color="yellow")
+
+	#Estilos de las aristas
+	nx.draw_networkx_edges(G, pos)
+
+	#Tamaño de los nombres de los nodos 
+	nx.draw_networkx_labels(G, pos, font_size=10)
+
+	#Agrega el peso a las aristas
+	edge_labels = nx.get_edge_attributes(G, "weight")
+	nx.draw_networkx_edge_labels(G,pos, edge_labels, font_size=6)
 
 
-#El sistema pide ingresar la cantidad de nodoso y luego las añade al grafo
+
+#El sistema pide ingresar la cantidad de nodoso y y relaciones
 a= int(input("Ingrese la cantidad de los nodos"))
 for i in range(a):
 	G.add_node(i)
+b= int(input("Ingrese la cantidad de relaciones "))
 
 #Creación aleatoria de aristas con Bernoulliy número aleatorios enteros para el peso de cada arista 
-p=0.3
-for node1 in G.nodes():
-	for node2 in G.nodes():
-		if node1<node2 and bernoulli.rvs(p=p):
-			G.add_weighted_edges_from([(node1,node2,random.randint(0, 100))])
+p=1
+contador=0
+while contador<b:
+	for node1 in G.nodes():
+		if(contador==b):
+				break
+		for node2 in G.nodes():
+			if node1<node2 and bernoulli.rvs(p=p):
+				if (not(G.has_edge(node1,node2))):
+					G.add_weighted_edges_from([(node1,node2,random.randint(0, 100))])
+					contador+=1
+					print("hola")
+			if(contador==b):
+				break		
 
 #contiene la posicion de cada nodo, con esta funcion ponemos una semilla para asi no se juntan tanto los nodos
-pos = nx.random_layout(G,seed=7)
+pos = nx.random_layout(G)
 
-#Estilos de los nodos
-nx.draw_networkx_nodes(G, pos, node_size=200,node_color="yellow")
-
-#Estilos de las aristas
-nx.draw_networkx_edges(G, pos)
-
-#Tamaño de los nombres de los nodos 
-nx.draw_networkx_labels(G, pos, font_size=10)
-
-#Agrega el peso a las aristas
-edge_labels = nx.get_edge_attributes(G, "weight")
-nx.draw_networkx_edge_labels(G,pos, edge_labels, font_size=6)
-
+#Llama a la funcion para establecer el formato del grafo
+AtributosDeGrafo(G,pos)
 
 #Muestra el grafo
 plt.show()
@@ -48,6 +63,8 @@ plt.show()
 #muestra las coordenadas del nodo 0 
 #Resultado [0.07630829 0.7799188 ] .Retorna un dict
 print(pos[0])
+print(pos[1])
+print(pos[2])
 
 #Muestra todo los nodos del grafico
 #Resultado[0, 1, 2]. Retorna un dict
@@ -68,4 +85,19 @@ print (G.__getitem__(0))
 
 #Para obtener el peso de la relación
 #Resultado Peso de la relacion entre 0 y 1:  {'weight': 35}
-print ("Peso de la relacion entre 0 y 1: ", G[0][1])
+#print ("Peso de la relacion entre 0 y 1: ", G[0][1])
+
+#Para gráficar los pasos del Algoritmo A*
+#Si queres agregar mas pasos tenes que agregarlos a la lista
+a=[0, 1, 2]
+a.append(4)
+H = G.subgraph(a)
+
+#Llama a la funcion para establecer el formato del grafo
+AtributosDeGrafo(H,pos)
+
+#Muestra el grafo
+plt.show()
+
+
+
